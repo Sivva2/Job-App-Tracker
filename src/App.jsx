4f390089613672216c8af4f298f582db158229c6
alexandrolastra <alexandrolastra.fr@gmail.com>
2024-09-27 18:07:27 +0200

    Alexandro edit on Navigation and create file and folders 27092024

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import "./global.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <>Ready to code with Mantine, a router would be nice in there 😺</>
+  return (
+    /* <>Ready to code with Mantine, a router would be nice in there 😺</>
+
+  /* Nav */
+
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/joblist" element={<JobListItem />} />
+        <Route path="/joblist/:jobId" element={<DetailJobItem />} />
+        <Route path="/joblist/:jobId" element={<EditJobItem />} />
+        <Route path="/statistics/:usedId" element={<DashBoardStatistics />} />
+        /* check if URL fits to our user Nav */
+        <Route path="*" element={<h1>404 Page</h1>} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
